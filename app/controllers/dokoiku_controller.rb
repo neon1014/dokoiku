2856af042d4c16bcb2c6f523ec4a3c5689c326b3
neon1014 <ridedefy1@live.jp>
2015-06-07 17:46:52 +0900

    隠し文字挿入関数を作成（未完成） ツイートする際の文字から「私は」を削除

--- app/controllers/dokoiku_controller.rb
+++ app/controllers/dokoiku_controller.rb
@@ -1,4 +1,7 @@
+# encoding : utf-8
 class DokoikuController < ApplicationController
+  require "openssl"
+  require "base64"
 
   def index
     #nilだったらエラー（正しくDBから値が返ってきてない）
@@ -29,14 +32,44 @@ class DokoikuController < ApplicationController
       _index_test(max+3)#=>NG
     end
 
+    #特定の文字列を表示する
+    def _decrypt
+      #暗号文読み込み
+      f = File::open("./app/controllers/hoge.txt","rb")
+      data = f.read
+      f.close
 
+      key = "hibike_euphonium"
+      iv =  "_kyoto_animation"
+
+      #複合化処理
+      cipher = OpenSSL::Cipher.new('aes-128-cbc')
+      cipher.decrypt
+      cipher.key = key
+      cipher.iv = iv
+
+      #150607
+      #wrong final block lengthが出てコンパイルできない
+      #正常に複合ができていない？
+      decrypted =  cipher.update(data) + cipher.final
+
+      return decrypted.to_s
+    end
+
+
+    #150607
+    #_decrypt関数が不具合のためコメントアウト
+    #複合文格納
+    #@decrypted = "!--"+_decrypt(data,key,iv).encode() + "--"
+
+    #StationDBからの値を格納
     @stations = Stations.all
 
     #randの範囲で動作することを確認済
     @random_id = Random.rand(2..@stations.count+1)
 
     #デバッグ用関数
-    _check_value_range(@stations.count)
+    #_check_value_range(@stations.count)
 
     @doko = Stations.find_by(id: @random_id)
 
